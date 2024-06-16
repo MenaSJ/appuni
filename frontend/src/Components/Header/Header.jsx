973b25fc6fcae716a07e1da4f98f3c87cfc38e29
Juan Mena <Mena_JS9@outlook.com>
2024-06-16 12:25:21 -0600

    update

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -3,6 +3,7 @@ import "./Header.css";
 const Header = () => {
     return (
         <div className="header">
+            <div className="opacity"></div>
             <div className="header-container">
                 <h2>Nuestro Objetivo</h2>
                 <p>UniExplore fue creada para Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur soluta, deleniti nesciunt dignissimos veniam autem similique eveniet iste quaerat assumenda quos distinctio, nostrum praesentium cupiditate quis impedit reprehenderit in. Culpa. </p>
