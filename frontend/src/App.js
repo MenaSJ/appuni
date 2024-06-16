20fb02aa304ee69c5ac39e0b2316a5b56ee1b2d6
Nelly Gonzalez <utp0148846@alumno.utpuebla.edu.mx>
2024-06-16 16:07:48 -0600

    Cambios 1.5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
