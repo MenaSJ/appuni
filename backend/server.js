4b3fbf82d52c56042325df60b52d68f8ae7ebeff
Juan Mena <Mena_JS9@outlook.com>
2024-07-02 12:31:52 -0600

    Simplificacion del server

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,98 @@
 const express = require('express');
-const app = express();
-const cors = require('cors')
+const cors = require('cors');
 require('dotenv').config();
+const mysql = require('mysql');
 
-const rutas = require('./routes/rutas');
+// Configuración de la base de datos
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
-// Middleware para parsear el cuerpo de las solicitudes
+db.connect((err) => {
+    if (err) {
+        console.error('Error al conectar a la base de datos:', err);
+    } else {
+        console.log('Conectado a la base de datos');
+    }
+});
+
+// Inicializar la aplicación Express
+const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Rutas
-app.use('/', rutas);
+// Definición de rutas y modelos
 
-// Inicia el servidor
+// Ruta para obtener carreras por universidad
+app.get('/universidades/:id/carreras', (req, res) => {
+    const universidadID = req.params.id;
+    const query = `
+        SELECT C.NombreCarrera
+        FROM Universidades U
+        JOIN Carreras C ON U.UniversidadID = C.UniversidadID
+        WHERE U.UniversidadID = ?;
+    `;
+    db.query(query, [universidadID], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(results);
+    });
+});
+
+// Ruta para obtener todas las universidades
+app.get('/universidades', (req, res) => {
+    const query = 'SELECT UniversidadID, Nombre, Acronimo, PaginaWeb, Vision, Mision, Logo FROM Universidades';
+    db.query(query, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(results);
+    });
+});
+
+// Iniciar el servidor
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor ejecutándose en http://localhost:${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+// const cors = require('cors')
+// require('dotenv').config();
+
+// const rutas = require('./routes/rutas');
+
+// // Middleware para parsear el cuerpo de las solicitudes
+// app.use(cors());
+// app.use(express.json());
+
+// // Rutasa
+// app.use('/', rutas);
+
+// // Inicia el servidor
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//     console.log(`Servidor ejecutándose en http://localhost:${port}`);
+// });
