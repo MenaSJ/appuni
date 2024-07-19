2efa1974e0ef13d1dec501a42c4ded055e4afeb4
Nelly Gonzalez <utp0148846@alumno.utpuebla.edu.mx>
2024-07-18 22:37:19 -0600

    Perfil agregado

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,11 +1,20 @@
+// Profile.js
 import "./Profile.css";
+import { useContext } from 'react';
+import { AppContext } from '../../context/Context';
 
 const Profile = () => {
+    const { user } = useContext(AppContext);
+
     return (
         <div className="perfil">
-            <p>Perfil del usuario</p>
+            <h2>Perfil del usuario</h2>
+            <p><strong>Nombre:</strong> {user.nombre}</p>
+            <p><strong>Apellido:</strong> {user.apellido}</p>
+            <p><strong>Estado:</strong> {user.estado}</p>
+            <p><strong>Email:</strong> {user.email}</p>
         </div>
     )
 }
 
-export default Profile
+export default Profile;
